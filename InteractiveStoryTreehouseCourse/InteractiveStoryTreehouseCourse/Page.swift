ee331e9842e4365adc548c2187b86fede241d0e9
Jesse Gay <jesse@teamtreehouse.com>
2019-05-01 17:58:49 -0700

    Added Page object and extensions
    The helper methods in the extension are really hard to follow. Hopefully they will make more sense once they are put into action.

--- InteractiveStoryTreehouseCourse/InteractiveStoryTreehouseCourse/Page.swift
+++ InteractiveStoryTreehouseCourse/InteractiveStoryTreehouseCourse/Page.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+class Page {
+    let story: Story
+    
+    // typealias allows for a more readable use of the values. This is just a tuple, but calling it Choice makes it read better.
+    typealias Choice = (title: String, page: Page)
+    
+    var firstChoice: Choice?
+    var secondChoice: Choice?
+    
+    init(story: Story) {
+        self.story = story
+    }
+}
+
+extension Page {
+    
+    
+    func addChoiceWith(title: String, page: Page) -> Page {
+        
+        switch (firstChoice, secondChoice) {
+        case(.some, .some): return self
+        case(.none, .none), (.none, .some): firstChoice = (title, page)
+        case (.some, .none): secondChoice = (title, page)
+        }
+        return page
+    }
+}
