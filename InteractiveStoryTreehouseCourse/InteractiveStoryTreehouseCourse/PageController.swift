1d42b786e4d95c4e4db2895a96d328962003417d
Jesse Gay <jesse@teamtreehouse.com>
2019-05-02 20:43:51 -0700

    Added first subview programmatically. Don't see it in Simulator
    Added the first subview programmatically. The app runs, but I don't see page.story.artwork.
    Added print statements to lines 38-39 of PageController.  It displays <UIImage: 0x600002aebb10>, {320, 568}, but I don't see image. ???

--- InteractiveStoryTreehouseCourse/InteractiveStoryTreehouseCourse/PageController.swift
+++ InteractiveStoryTreehouseCourse/InteractiveStoryTreehouseCourse/PageController.swift
@@ -12,6 +12,13 @@ class PageController: UIViewController {
     
     var page: Page? //Pasan goes on at length explaining why this is an optional, even though we always require it, but the explanation was lost on me.
     
+    // MARK: - User Interface Properties
+    
+    let artworkView = UIImageView()
+    let storyLabel = UILabel()
+    let firstChoiceButton = UIButton(type: .system)
+    let secondChoiceButton = UIButton(type: .system)
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -24,11 +31,38 @@ class PageController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        if let page = page {
+            artworkView.image = page.story.artwork // I don't see an image in Simulator.
+            print(page.story.text) // testing to make sure story is being presented
+            print(page.story.artwork)// Prints <UIImage: 0x600002aebb10>, {320, 568} so I think it's trying, but somehow it's not rendering the image properly.
+        }
         // Do any additional setup after loading the view.
     }
     
-    // MARK: - Navigation
+    // When ViewController's view is initialized, it checks to see if it has any subviews. It includes the viewDidLayoutSubviews method, which, by default, does nothing, but we can override it to add subviews, that the View will create as it cycles through it's subview setup.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews() // notice we call the superclass first, and we've already called super.viewDidLoad()
+        view.addSubview(artworkView) // This is how we add a subview.
+        artworkView.translatesAutoresizingMaskIntoConstraints = false // This overrides autolayouts inferred constraints.
+        
+        //  artworkView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true // this sets the topAnchor of the artworkView subView to the topAnchor of the View, and makes it active. We could do this for every parameter, but it would get tedious. It's more efficient to use NSLayoutContraint.activate, and pass in an array of constraints. This is how we activate and constrain the subview we just added.
+        NSLayoutConstraint.activate([
+            artworkView.topAnchor.constraint(equalTo: view.topAnchor),
+            artworkView.bottomAnchor.constraint(equalTo: view.topAnchor),
+            artworkView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            artworkView.leftAnchor.constraint(equalTo: view.leftAnchor)
+            ])
+    }
     
 
 }
+
+
+
+
+
+
+
+
+
+
--- InteractiveStoryTreehouseCourse/InteractiveStoryTreehouseCourse/ViewController.swift
+++ InteractiveStoryTreehouseCourse/InteractiveStoryTreehouseCourse/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // this method is called on our ViewController subclass when the segue is fired off. By default it does nothing, but in a ViewController subclass we can use it to configure the DESTINATION ViewController prior to it being displayed. Every segue has a string identifier (by default it's blank) so select the segue, click on the Attributes inspector, and give it a name. Then we can refer to it specifically in subsequent methods.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "startAdventure" {
+            guard let pageController = segue.destination as? PageController else {
+                return
+            }
+            pageController.page = Adventure.story
+        }
+    }
 }
 
