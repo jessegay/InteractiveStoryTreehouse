496b07faf014f03bcccec1206be30d339b5412ed
Jesse Gay <jesse@teamtreehouse.com>
2019-05-06 20:48:54 -0700

    Added attributed text, choice buttons

--- InteractiveStoryTreehouseCourse/InteractiveStoryTreehouseCourse/PageController.swift
+++ InteractiveStoryTreehouseCourse/InteractiveStoryTreehouseCourse/PageController.swift
@@ -33,7 +33,26 @@ class PageController: UIViewController {
         
         if let page = page {
             artworkView.image = page.story.artwork
-            storyLabel.text = page.story.text
+            // Let's style the text. Need to create NSMutableAttributedString then modify its properties.
+            let attributedString = NSMutableAttributedString(string: page.story.text)
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = 10
+            
+            attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+            
+            storyLabel.attributedText = attributedString // Changed from storyLabel.text to .attributedText in order to accept  attributedString (which is instance of NSMutableAttributedString)
+            
+            if let firstChoice = page.firstChoice {
+                firstChoiceButton.setTitle(firstChoice.title, for: .normal)
+            } else {
+                firstChoiceButton.setTitle("Play Again", for: .normal)
+            }
+            
+            if let secondChoice = page.secondChoice {
+                secondChoiceButton.setTitle(secondChoice.title, for: .normal)
+            } 
+            
         }
         // Do any additional setup after loading the view.
     }
@@ -61,7 +80,22 @@ class PageController: UIViewController {
             storyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
             storyLabel.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -48.0)
             ])
+        
+        view.addSubview(firstChoiceButton)
+        firstChoiceButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            firstChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80.0)
+            ])
        
+        view.addSubview(secondChoiceButton)
+        secondChoiceButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            secondChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32.0)
+            ])
     }
 }
 
